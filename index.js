7d42251854bec302474ca58d3190a1dc180c83e2
Abhas Gaur <97803728+Abhas009@users.noreply.github.com>
2024-03-07 23:41:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+var level = 0;
+
+var gameStarted = false;
+
+// Listen for keydown events on the document
+$(document).keydown(function(event) {
+  if (!gameStarted) {
+    $("#level-title").html("Level" + level);
+    nextSequence();
+    gameStarted = true;
+  }
+});
+
+$(".btn").on("click",function()
+{
+   var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+
+})
+
+//Create a new Function called nextSequence()
+function nextSequence()
+{
+  //random number
+  var randomNumber = Math.floor(Math.random(randomNumber) * 4);
+
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+
+//Sound Play
+function playSound(name)
+{
+  //Play Sound
+  var sound = new Audio("sounds/" + name + ".mp3");
+  sound.play();
+}
+
+
+//animate
+function animatePress(currentColour)
+{
+  $("#" + currentColour).addClass("pressed")
+
+  setTimeout(() =>
+  {
+    $("#" + currentColour).removeClass("pressed");
+  },100)
+}
